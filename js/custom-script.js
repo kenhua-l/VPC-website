d040b61ffd05ba38ac7fee13720735ff8926427d
Ken <l_kenhua93@hotmail.com>
2015-10-19 14:05:43 +0800

    A sort of working page

--- js/custom-script.js
+++ js/custom-script.js
@@ -20,20 +20,6 @@ function update(){
     });
 };
 
-// $(document).ready(function(){
-// 	$('#nav').localScroll(800);
-	
-// 	//.parallax(xPosition, speedFactor, outerHeight) options:
-// 	//xPosition - Horizontal position of the element
-// 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
-// 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-// 	$('#about').parallax("50%", 0.1);
-// 	$('#team').parallax("50%", 0.1);
-// 	//$('.bg').parallax("50%", 0.4);
-// 	$('#portfolio').parallax("50%", 0.3);
-
-// })
-
 function hasClass(element, cls){
 	return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 };
